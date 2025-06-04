81e5a4fa4d43532abb3987d87d3e40eb1b957b79
alberto <jesusalbertojimenez500@gmail.com>
2025-06-04 14:56:33 -0600

    new model users

---
+++ Model/Usuario/Usuario.py
@@ -0,0 +1,8 @@
+class Usuario():
+    def __init__(self, nombre, usuario, contraseña, edad, matricula):
+        self.nombre = nombre
+        self.usuario =  usuario
+        self.contraseña = contraseña
+        self.edad = edad
+        self.matricula = matricula
+    
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+from Model.Usuario.Usuario import Usuario
 app = Flask(__name__)
 
 
@@ -7,5 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template('inicio.html')
 
+
+@app.route('/password/<password>/', methods=["GET"])
+def password(password):
+    return f'Tu passwor is: {generate_password_hash(password)}'
+
+@app.route('/usuario', methods=["GET"])
+def usuario():
+    pass
 if __name__ == '__main__':
     app.run(debug=True)
